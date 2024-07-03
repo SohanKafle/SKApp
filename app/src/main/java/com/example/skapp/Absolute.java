f5f5353a553129da85fd926333a4e2a32b6362e3
SohanKafle <soka_bca2077@lict.edu.np>
2024-07-03 18:03:20 +0545

    commit

---
+++ app/src/main/java/com/example/skapp/Absolute.java
@@ -0,0 +1,14 @@
+package com.example.skapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Absolute extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register5);
+    }
+}
